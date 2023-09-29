8f44a96dbc55ec61da42b1480e61eeeb3f0f1465
SamianSaja <oteotegodok1945@gmail.com>
2023-09-29 23:08:36 +0700

    tinggal melengkapi dan animasi sebagus mungkin

---
+++ script.js
@@ -0,0 +1,39 @@
+function showHome() {
+    document.getElementById('home').style.display = 'block'
+    document.getElementById('about').style.display = 'none'
+    document.getElementById('services').style.display = 'none'
+    document.getElementById('my-project').style.display = 'none'
+    document.getElementById('contact').style.display = 'none'
+}
+
+function showAbout() {
+    document.getElementById('about').style.display = 'grid'
+    document.getElementById('home').style.display = 'none'
+    document.getElementById('services').style.display = 'none'
+    document.getElementById('my-project').style.display = 'none'
+    document.getElementById('contact').style.display = 'none'
+}
+
+function showServices() {
+    document.getElementById('services').style.display = 'flex'
+    document.getElementById('about').style.display = 'none'
+    document.getElementById('home').style.display = 'none'
+    document.getElementById('my-project').style.display = 'none'
+    document.getElementById('contact').style.display = 'none'
+}
+
+function showMyProject() {
+    document.getElementById('my-project').style.display = 'flex'
+    document.getElementById('about').style.display = 'none'
+    document.getElementById('services').style.display = 'none'
+    document.getElementById('home').style.display = 'none'
+    document.getElementById('contact').style.display = 'none'
+}
+
+function showContact() {
+    document.getElementById('contact').style.display = 'flex'
+    document.getElementById('about').style.display = 'none'
+    document.getElementById('services').style.display = 'none'
+    document.getElementById('my-project').style.display = 'none'
+    document.getElementById('home').style.display = 'none'
+}
